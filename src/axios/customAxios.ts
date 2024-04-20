2ffd501fde3862e9d311c5eb44c58f6c5d56e577
Tanmay070501 <xyzabc.am89@gmail.com>
2024-04-21 04:00:41 +0530

    token expire or invalid handling

--- src/axios/customAxios.ts
+++ src/axios/customAxios.ts
@@ -7,15 +7,36 @@ customAxios.interceptors.request.use(
     function (config) {
       
       const token = useUserStore.getState().authToken
+      
+      if(!token) {
+        console.log("Token missing");
+        return Promise.reject('Token is missing');
+      }
       if (token) {
         // If token exists, add Authorization header to the request
         config.headers['Authorization'] = `Bearer ${token}`;
-      }
+      } 
       return config;
     },
     function (error) {
       return Promise.reject(error);
     }
-  );
+);
+
+
+customAxios.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    if (error.response && error.response.status === 401) {
+      // Perform logout action here
+      console.log('Logout due to 401 error');
+      const logout = useUserStore.getState().logout
+      logout();
+    }
+    return Promise.reject(error);
+  }
+);
 
 export {customAxios}
