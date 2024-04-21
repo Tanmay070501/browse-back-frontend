cbd63445a70372678107b79e0b3cf76970aa78f7
Tanmay070501 <xyzabc.am89@gmail.com>
2024-04-21 18:05:09 +0530

    snippet update

--- src/constants/snippet.ts
+++ src/constants/snippet.ts
@@ -1,5 +1,18 @@
+import { BASE_URL } from "@/urls/urlProvider"
+
 export const setupCodeSnippet = (apiKey: string = 'your api key here') => {
     return (
-        `import { BrowseBack } from "@tannu-dev/browse-back";\nBrowseBack.init({\n\t\tapiKey: '${apiKey}'\n});`
-    )
+`import { BrowseBack } from "@tannu-dev/browse-back";
+
+BrowseBack.init({
+    apiKey: '${apiKey}',
+    recordErrorOnly: false,
+    socketUrl: '${(BASE_URL ?? "").replace("http", "ws")}',
+    recordConsole: true,
+},{
+    recordCanvas: true,
+    recordCrossOriginIframes: true,
+});
+`
+)
 }
